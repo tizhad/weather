b5e285eef52531595255e49d1e9d396f945ad591
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-02-26 01:28:02 +0100

    AppHeader component added.

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Weather Forecast</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <button type="button" class="btn btn-light" @click="$emit('showModal')">Add City</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  methods: {
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
