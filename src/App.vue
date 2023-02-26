5b1a8f825c47b3a6ba830d6486425512b16ef321
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-02-26 23:06:00 +0100

    Get Default city forecast data when user opens application

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <AppHeader @showSearchCityForm="showAddCityForm"></AppHeader>
   <SearchCity v-if="showForm" @errorMessage="showAlert"  @newQuery = removeAlert></SearchCity>
+  <CityForecast v-if="defaultCityForecast" :defaultCityData="defaultCityForecast"></CityForecast>
   <AlertMessage v-if="showErrorMsg" :message=errorMsg></AlertMessage>
+
 </template>
 
 <script>
 import AlertMessage from '@/components/AlertMessage.vue';
+import CityForecast from '@/components/CityForecast.vue';
 import SearchCity from '@/components/SearchCity.vue';
 import AppHeader from '@/components/AppHeader.vue';
+import { getDefaultCityForecast } from "@/api/weatherApi";
 
 export default {
   name: 'App',
   components: {
+    CityForecast,
     AlertMessage,
     SearchCity,
     AppHeader,
@@ -21,7 +26,11 @@ export default {
       showForm: false,
       showErrorMsg: false,
       errorMsg: "",
+      defaultCityForecast: null,
     };
+  },
+  created() {
+  this.getDefaultCityDate();
   },
   methods: {
     showAddCityForm() {
@@ -36,7 +45,20 @@ export default {
     },
     removeAlert() {
       this.showErrorMsg = false;
-    }
+    },
+    async getDefaultCityDate() {
+        try {
+          const result = await getDefaultCityForecast();
+          if (result && result.list.length > 0) {
+            this.defaultCityForecast = result;
+          }
+          if (this.defaultCityForecast === null) {
+            this.errorMsg = "Something went wrong! Please refresh the page."
+          }
+        } catch (error) {
+          this.errorMsg = "Something went wrong! Please refresh the page."
+        }
+      },
   }
 }
 </script>
