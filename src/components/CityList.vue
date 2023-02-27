100af2825baa7bc5af746a9964276dfefef6e7b2
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-02-27 18:08:40 +0100

    Added CityList component to show the cities list.

---
+++ src/components/CityList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="row">
+    <div class="col-md-6">
+      <TodayForecast
+        v-if="newCityForecast"
+        :forecastData="cityData"
+      ></TodayForecast>
+    </div>
+    <div class="col-md-6">
+      <TomorrowForecast
+        v-if="newCityForecast"
+        :forecast="cityData"
+      ></TomorrowForecast>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodayForecast from "@/components/TodayForecast.vue";
+import TomorrowForecast from "@/components/TomorrowForecast.vue";
+
+export default {
+  name: "CityList",
+  components: { TomorrowForecast, TodayForecast },
+  props: {
+    newCityForecast: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    console.log(this.newCityForecast, "new city in city list");
+  },
+  data() {
+    return {
+      cityData: {
+        city: this.newCityForecast.city.name,
+        temp: this.newCityForecast.list[0].main.temp,
+        feels_like: this.newCityForecast.list[0].main.feels_like,
+        temp_min: this.newCityForecast.list[0].main.temp_min,
+        temp_max: this.newCityForecast.list[0].main.temp_max,
+        humidity: this.newCityForecast.list[0].main.humidity,
+        windSpeed: this.newCityForecast.list[0].wind.speed,
+        description: this.newCityForecast.list[0].weather[0].description,
+        icon: `http://openweathermap.org/img/wn/${this.newCityForecast.list[0].weather[0].icon}@2x.png`,
+      },
+    };
+  },
+  methods: {},
+};
+</script>
