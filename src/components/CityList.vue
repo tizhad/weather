a0c3b956522bee1cecde06649986240ee4456a6b
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-03-03 00:17:23 +0100

    Modify city list component to show forecast data.

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -1,51 +1,46 @@
 <template>
   <div class="row">
-    <div class="col-md-6">
+    <div
+      class="col-md-6"
+      v-for="(item, index) in this.todayForecast"
+      :key="index"
+    >
       <TodayForecast
-        v-if="newCityForecast"
-        :forecastData="cityData"
+        v-if="item"
+        :forecastData="item"
       ></TodayForecast>
-    </div>
-    <div class="col-md-6">
-      <TomorrowForecast
-        v-if="newCityForecast"
-        :forecast="cityData"
-      ></TomorrowForecast>
     </div>
   </div>
 </template>
 
 <script>
 import TodayForecast from "@/components/TodayForecast.vue";
-import TomorrowForecast from "@/components/TomorrowForecast.vue";
+
+import { toRaw } from "vue";
 
 export default {
   name: "CityList",
-  components: { TomorrowForecast, TodayForecast },
+  components: { TodayForecast },
   props: {
-    newCityForecast: {
+    cityForecast: {
       type: Object,
       required: true,
     },
   },
-  mounted() {
-    console.log(this.newCityForecast, "new city in city list");
-  },
   data() {
     return {
-      cityData: {
-        city: this.newCityForecast.city.name,
-        temp: this.newCityForecast.list[0].main.temp,
-        feels_like: this.newCityForecast.list[0].main.feels_like,
-        temp_min: this.newCityForecast.list[0].main.temp_min,
-        temp_max: this.newCityForecast.list[0].main.temp_max,
-        humidity: this.newCityForecast.list[0].main.humidity,
-        windSpeed: this.newCityForecast.list[0].wind.speed,
-        description: this.newCityForecast.list[0].weather[0].description,
-        icon: `http://openweathermap.org/img/wn/${this.newCityForecast.list[0].weather[0].icon}@2x.png`,
-      },
+      todayForecast: null,
+      tomorrowForecast: null,
+      cityForecastData: [],
     };
   },
-  methods: {},
+  created() {
+    this.setTodayForecast(this.cityForecast);
+  },
+  methods: {
+    setTodayForecast(forecast) {
+      this.todayForecast = toRaw(forecast);
+    },
+  },
 };
 </script>
