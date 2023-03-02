6ad609ffee4031a6bcc0d1be185be61261db9981
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-03-03 00:21:10 +0100

    Modify search component to get correct data to show.

--- src/components/SearchCity.vue
+++ src/components/SearchCity.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="row mb-5 mt-5">
     <div class="col-lg-8 mx-auto">
-      <form @submit.prevent="getCityForecast">
-          <div class="input-group">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search City ..."
-              aria-label="Search"
-              v-model="query"
-            />
-            <div class="input-group-prepend">
-              <button
-                type="submit"
-                class="btn btn-danger rounded text-warning"
-                @click="getCityForecast"
-              >
-                <i class="fa fa-search"></i>
-              </button>
-            </div>
+      <form @submit.prevent="getSearchResult">
+        <div class="input-group">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search City ..."
+            aria-label="Search"
+            v-model="query"
+          />
+          <div class="input-group-prepend">
+            <button
+              type="submit"
+              class="btn btn-danger rounded text-warning"
+            >
+              <i class="fa fa-search"></i>
+            </button>
           </div>
+        </div>
       </form>
     </div>
   </div>
   <div v-if="loading" class="spinner-border text-primary" role="status">
     <span class="sr-only">Loading...</span>
   </div>
+  <div v-if="message" class="alert alert-danger w-50 align-items-center align-self-center m-auto" role="alert">{{message}}</div>
 </template>
 
 <script>
@@ -33,36 +33,45 @@ import { getWeatherData } from "@/api/weatherApi";
 
 export default {
   name: "SearchCity",
-  emits: ["errorMessage", "newQuery", "newSearch"],
+  emits: ["newSearch"],
   data() {
     return {
       query: "",
       loading: false,
-      searchResults: [],
+      searchResult: null,
       message: "",
     };
   },
   watch: {
     query(newVal, oldVal) {
-      if(newVal !== oldVal ) this.$emit("newQuery");
-    }
+      if (newVal !== oldVal) {
+        this.message = null;
+      }
+    },
   },
   methods: {
-    async getCityForecast() {
+    async getSearchResult() {
       this.loading = true;
       try {
         const result = await getWeatherData(this.query);
-        this.searchResults = result;
-        this.$emit("newSearch", this.searchResults);
-        this.loading = false
-        if (this.searchResults === null) {
-          this.$emit("errorMessage", "Please Enter the city name correctly!")
-          this.loading = false;
+        this.searchResult = result;
+        if (this.searchResult === null) {
+          this.message = "Please Enter the city name correctly!";
+        }else {
+          this.setData(this.searchResult);
         }
       } catch (error) {
-        this.$emit("Please enter the city name correctly!", error)
+        this.message = "Something went wrong, Please try again"
+      } finally {
+        this.loading = false;
       }
     },
+    setData(searchResult) {
+      const cityForecast =  {
+        city: searchResult.city.name,
+      };
+      this.$emit("newSearch", cityForecast.city);
+    }
   },
 };
 </script>
