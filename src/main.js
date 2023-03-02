0c069db4c68bc7392e5c5786807f4a56550946fd
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-03-03 00:20:25 +0100

    Fix compilation error.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.mount("#app");
